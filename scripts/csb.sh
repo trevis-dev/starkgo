3cf2c6e9744246df3fb4c30692b805fb0f7cd6ab
Florian Charlier <98014814+trevis-dev@users.noreply.github.com>
2024-06-16 13:06:59 +0200

    fix set_blakc and add scripts

---
+++ scripts/csb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+pushd $(dirname "$0")/..
+
+export RPC_URL="http://localhost:5050";
+
+export WORLD_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.world.address')
+
+# sozo execute --world <WORLD_ADDRESS> <CONTRACT> <ENTRYPOINT>
+sozo execute --world $WORLD_ADDRESS starkgo::systems::actions::actions set_black -c 1,1 --wait
